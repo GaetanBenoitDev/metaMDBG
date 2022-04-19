fd4bb13b60e729d44dbe7c8f1dd1877bdf896787
gats <gae.benoit@gmail.com>
2022-04-19 13:17:29 +0100

    add mapping program

--- src/mapping/Mapping_BinReads.hpp
+++ src/mapping/Mapping_BinReads.hpp
@@ -219,7 +219,7 @@ public:
 
 
 		u_int64_t readIndex = read._index;
-		cout << readIndex << " " << _kmervec_to_contigIndex.size() << endl;
+		//cout << readIndex << " " << _kmervec_to_contigIndex.size() << endl;
 
 		string rleSequence;
 		vector<u_int64_t> rlePositions;
@@ -239,10 +239,12 @@ public:
 			if(_kmervec_to_contigIndex.find(vec) == _kmervec_to_contigIndex.end()) continue;
 
 			for(u_int32_t binIndex : _kmervec_to_contigIndex[vec]){
-				cout << "lala: " << binIndex << endl;
+				//cout << "lala: " << binIndex << endl;
 				ofstream& file = _binFiles[binIndex];
+				cout << file.is_open() << " " << read._header << endl;
 				file << ">" << read._header << endl;
 				file << read._seq << endl;
+				file.flush();
 			}
 
 			/*
